53f5807d9338fcb2e45249f0694feee9206eeaf3
kozlov721isip <145563641+kozlov721isip@users.noreply.github.com>
2023-11-08 18:55:15 +0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,58 @@
+"use strict";
+/*
+let date = new Date();
+console.log(date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate())
+*/
+
+/*
+let str = '2025-12-31';
+let arr = str.split('-');
+let res = arr[2] + '/' + arr[1] + '/' + arr[0];
+
+console.log(res);
+*/
+
+/*
+let date = new Date();
+console.log(date.getDay());
+*/
+
+/*
+let date = new Date();
+let day  = date.getDay();
+let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+console.log(days[day]);
+*/
+
+/*
+let date = new Date();
+let month = date.getMonth();
+let months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн','июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
+
+console.log(months[month])
+*/
+
+/*
+let date = new Date(2005, 10, 15); 
+let day  = date.getDay();
+let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+console.log(days[day]);
+*/
+
+/*
+let date = new Date(2005, 10, 15, 23, 59, 59);
+console.log(date.getTime());
+*/
+
+/*
+let date = new Date(2020, 2, 0);
+console.log(date.getDate());
+*/
+
+/*
+let now  = new Date();
+let date = new Date(now.getFullYear() + 1, 0, 1);
+console.log(date)
+*/
